d4e4cb30b7a4f1d1d59158c71f6249f7a2f1dc0d
Sean Holden <seanholden1@gmail.com>
2017-08-27 22:08:38 +0100

    removed unnecessary default git repo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,16 @@
 default['dev_env']['user'] = 'vagrant'
+
 default['dev_env']['ohmyzsh_plugins'] = %w(git)
-default['dev_env']['repos'] = %w(
-  git@github.com:SeanHolden/ReactReduxStarter.git
-)
+
+default['dev_env']['repos'] = %w()
+
 default['dev_env']['environment_variables'] = %w(
   EDITOR="vim"
 )
+
 default['dev_env']['aliases'] = [
   'befs="bundle exec foreman start"',
   'be="bundle exec"',
 ]
+
 default['dev_env']['misc_lines'] = []
